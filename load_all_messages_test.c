ed3b8489748f6f2563075d89b3b7e1e951af151f
irshad627 <irshadmohamedwork@gmail.com>
2025-04-24 18:28:11 -0400

    Add files via upload

---
+++ load_all_messages_test.c
@@ -0,0 +1,36 @@
+//a C header file that provides functions for input and output operations, such as reading from and writing to files and the console.
+#include <stdio.h>
+//a C header file that provides functions for memory allocation, process control, conversions, and other utility operations.
+#include <stdlib.h>
+//a C header file that provides functions for manipulating C-style strings and memory blocks.
+#include <string.h>
+
+//Struct for each record
+typedef struct {
+    int ID;
+    char title[50];
+    char message[288];
+} Record;
+
+/*
+*Load Messages Function
+iterates throughout the dat file and prints the data stored within the 
+structs.
+*/
+int load_messages(){
+    FILE *fptr;
+
+    //Opens file in "read binary mode"
+    fptr = fopen("records.dat", "rb");   
+
+    //placeholder record
+    Record rec;
+    //iterates through all the structs in the dat file and prints the ID,title and message
+    while (fread(&rec, sizeof(Record), 1, fptr)){
+        printf("ID: %d\n", rec.ID);
+        printf("Title: %s\n", rec.title);
+        printf("Message: %s\n\n", rec.message);
+    }
+    fclose(fptr);  
+    return 0;
+}
