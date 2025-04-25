bf13b61ba6e743f0937bb19740211ebcbb89d801
ser-gar <serina.r.garrett@gmail.com>
2025-04-25 18:49:54 -0400

    Create edit_modify_messages.c
    Edit/Modify Function

---
+++ src/optional_features(temporary)/edit_modify_messages.c
@@ -0,0 +1,55 @@
+#include "message.h"
+#include <stdio.h>
+#include <string.h>
+
+void editMessage() {
+    
+    int editIndex;
+    printf("\nEnter the message index of the message you want to edit: ");
+    scanf("%d", &editIndex);
+    getchar(); // consume newline character
+
+    if (editIndex < 0 || editIndex >= totalMessages) {
+        printf("Invalid index entered.\n");
+        return;
+    }
+
+    Message *msg = &messages[editIndex];
+
+    if (msg->isEncrypted) {
+        char pass[MAX_LEN];
+        printf("This message is encrypted. Please enter the password to decrypt it: ");
+        fgets(pass, MAX_LEN, stdin);
+        pass[strcspn(pass, "\n")] = 0;
+        
+        if (strcmp(pass, PASS) != 0) {
+    printf("Incorrect password. You do not have access to edit this message.\n");
+    return;
+}
+
+//This block of code will display the message, ask the user for the modified version and then update it
+    printf("Please see the Decrypted Message below: %s\n", msg->decryptedMessage);
+    printf("Enter the new information to be added: ");
+    
+    char newInfo[MAX_LEN];
+    fgets(newInfo, MAX_LEN, stdin);
+    newInfo[strcspn(newInfo, "\n")] = 0;
+    strcpy(msg->decryptedMessage, newInfo);
+    printf("Your message was updated!:)");
+    
+//This will do the same as above but for the plain text messages
+    
+} else {
+    printf("Please see your message below: %s\n", msg->content);
+    printf("Enter the new information to be added: ");
+    
+    char newInfo[MAX_LEN];
+    fgets(newInfo, MAX_LEN, stdin);
+    newInfo[strcspn(newInfo, "\n")] = 0;
+    strcpy(msg->content, newInfo);
+    printf("Your message was updated!:)");
+    
+    
+    }
+
+}
