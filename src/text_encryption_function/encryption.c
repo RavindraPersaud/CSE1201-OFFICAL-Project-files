326bce338d730623906020cb1c8b121c086b2cdf
DinMoh <dinarmohabir07@gmail.com>
2025-04-25 00:16:04 -0400

    updated documentation
    .

--- src/text_encryption_function/encryption.c
+++ src/text_encryption_function/encryption.c
@@ -6,38 +6,40 @@
 #include <stdlib.h>
 
 /*
-*Columnar Transpositon Cipher
-*method of encryption where the message is written out in rows under a keyword, 
-*then the columns are rearranged in the order of the letters in the keyword (usually alphabetically). 
-*The ciphertext is then read column by column.
+Columnar Transpositon Cipher
+method of encryption where the message is written out in rows under a keyword, 
+then the columns are rearranged in the order of the letters in the keyword (usually alphabetically). 
+The ciphertext is then read column by column.
 */
+
+// Encrytps the message given by the user.
 char* encrypt(char message[], char key[]){
-    //Getting message and key length for array iteration
+    //Getting message and key length for array iteration.
     int msg_len = strlen(message);
     int key_len = strlen(key);
 
-    //Counter to keep track of place in plaintext(message)
+    //Counter to keep track of place in plaintext(message).
     int counter = 0;
 
-    //variable for return value (uses malloc because local arrays get detsroyed after function execution)
+    //variable for return value (uses malloc because local arrays get detsroyed after function execution).
     char* full_sentence = malloc((msg_len + key_len + 1) * sizeof(char));
     full_sentence[0] = '\0'; 
 
-    //Calculating rows based on message length and remainder
+    //Calculating rows based on message length and remainder.
     int rows = (msg_len / key_len) + (msg_len % key_len != 0);
 
     char matrix[rows][key_len];
     char matrix2[key_len][rows + 1];
 
-    //vars used for sorting and swapping
+    //vars used for sorting and swapping.
     char temp;
     char matrix2_temp[rows + 1];
     
-    //copy of key as editing key in place causes errors
+    //copy of key as editing key in place causes errors.
     char mod_key[key_len + 1]; 
     strcpy(mod_key, key);
     
-    //Constructing matrixs with dimensions rows and key_len replacing spaces with '-'
+    //Constructing matrixs with dimensions rows and key_len replacing spaces with '-'.
     for(int i = 0; i<rows; i++){
         for(int j = 0; j < key_len; j++){
             if ((counter) < msg_len){
@@ -62,15 +64,15 @@ char* encrypt(char message[], char key[]){
         }
     }
 
-    //SORTING
+    //SORTING.
     for(int k = 0; k < key_len ; k++){
-        //Uses selection sorting to sort based on the ASCII value of each char in key
+        //Uses selection sorting to sort based on the ASCII value of each char in key.
         for (int l = k+1; l < key_len ; l++){
             if((int)mod_key[l] < (int)mod_key[k]){
                 temp = mod_key[l];
                 mod_key[l] = mod_key[k];
                 mod_key[k] = temp;
-                //sorts matrix as key is being sorted
+                //sorts matrix as key is being sorted.
                 strcpy(matrix2_temp, matrix2[l]);
                 strcpy(matrix2[l],matrix2[k]);
                 strcpy(matrix2[k], matrix2_temp);
