f7c6263d5130138102b65212f6fe603cbb522a2b
RavindraPersaud <persaudr788@gmail.com>
2025-04-26 13:56:58 -0400

    Update delete_message.c

--- src/delete_message.c
+++ src/delete_message.c
@@ -1,3 +1,13 @@
+/* 
+delete_message.c
+----------------------------
+Offers a user-friendly interface for message removal.
+enables users to remove messages by ID or title and examine all message IDs. 
+This module connects menu interactions with the backend file functions 
+defined in storage.c.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,24 +21,30 @@ typedef struct {
 
 
 /*
-Delete Record by ID Function
-iterates through all Record.ID in the .dat file and deletes them
+delete_rec_by_id
+--------------------------------
+Deletes a record from "records.dat" by its unique ID.
+Steps: 
+1.Opens temporary file to store all records except the one with the given ID
+2.Writes all records except the on with the given ID to the temperary file
+3.Deletes old file containing the needed to be deleted record
+4.Renames temperary file to old file's name
+5.confirmation message
+Returns 0 on successful deletion, 1 on failure.
 */
 int delete_rec_by_id(int id){
-    //Opens file in "read binary" mode
-    FILE *fptr = fopen("records.dat", "rb");
 
-    //Opens temperary file to store all records except the one with the given ID
+    FILE *fptr = fopen("records.dat", "rb");
+   
     FILE *temp = fopen("temp.dat", "wb");
 
     if (fptr == NULL || temp == NULL){
         printf("Error opening files.\n");
         return 1;
     }
-    //placeholder record
+    
     Record rec;
-
-    //Writes all records except the on with the given ID to the temperary file
+   
     while (fread(&rec, sizeof(Record), 1, fptr)){
         if (rec.ID != id) {
             fwrite(&rec, sizeof(Record), 1, temp);
@@ -37,38 +53,41 @@ int delete_rec_by_id(int id){
 
     fclose(fptr);
     fclose(temp);
-
-    //Deletes old file containing the needed to be deleted record
+    
     remove("records.dat");
-
-    //renames temperary file to old file's name
+    
     rename("temp.dat", "records.dat");
 
-    //confirmation message
     printf("Record with ID %d was removed.\n", id);
     return 0;
 }
 
 
 /*
-Delete Record by Title Function
-iterates through all Record.title in the .dat file and deletes them
+delete_rec_by_title
+--------------------------
+Deletes a record from "records.dat" by matching the title.
+Steps:
+1.Opens file in "read binary" mode
+2.Opens temporary file to store all records except the one with the given title.
+3.Writes all records except the on with the given title to the temporary file.
+4.Deletes old file containing the needed to be deleted record.
+5.Renames temperary file to old file's name.
+6.Confirmation message.
+Iterates through all Record.title in the .dat file and deletes them.
+Returns 0 on successful deletion, 1 on failure.
 */
 int delete_rec_by_title(char title[]){
-    //Opens file in "read binary" mode
     FILE *fptr = fopen("records.dat", "rb");
 
-    //Opens temperary file to store all records except the one with the given title
     FILE *temp = fopen("temp.dat", "wb");
 
     if (fptr == NULL || temp == NULL){
         printf("Error opening files.\n");
         return 1;
     }
-    //placeholder rec
     Record rec;
-
-    //Writes all records except the on with the given title to the temperary file
+    
     while (fread(&rec, sizeof(Record), 1, fptr)){
         if (strcmp(rec.title, title) != 0){
             fwrite(&rec, sizeof(Record), 1, temp);
@@ -77,14 +96,11 @@ int delete_rec_by_title(char title[]){
 
     fclose(fptr);
     fclose(temp);
-
-    //Deletes old file containing the needed to be deleted record 
+    
     remove("records.dat");
-
-    //renames temperary file to old file's name
+   
     rename("temp.dat", "records.dat");
 
-    //confirmation message
     printf("Record with Title %s removed.\n", title);
     return 0;
 }
