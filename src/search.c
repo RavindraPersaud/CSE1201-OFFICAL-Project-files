90ce03b6e92d9e2a39b693dad6eb42c21ce90e51
irshad627 <irshadmohamedwork@gmail.com>
2025-04-28 22:05:42 -0400

    Update search.c

--- src/search.c
+++ src/search.c
@@ -1,49 +1,49 @@
 /*
 search_functions.c
-
+-----------------------------------------
 Functions to search message records stored in 'records.dat'.
 Allows search by:
 - Phrase (substring in message)
 - Title (exact match)
 - ID (exact match)
-
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-/* 
-Struct representing a record.
-*/
+// Struct representing a record
 typedef struct {
     int ID;
+    int is_encrypted;
     char title[50];
     char message[288];
 } Record;
 
+char* decrypt(char message[], char key[]);
+void ask_decrypt(Record rec);
+
 
 /*
 search_by_phrase
-Searches through all the messages in the structs to see if any message contains the given word/phrase.
-
-Opens file in "read binary" mode.
+---------------------------
+Searches through all the messages in the structs to see if any messages contains the given word/phrase.
+opens file in "read binary" mode
 Prints all matching records.
 Returns 0 on success, 1 on error.
 */
-int search_by_phrase() {
 
+void search_by_phrase() {
+    int found = 0;
     char phrase[288];
-
     printf("Enter Phrase:");
     scanf(" %[^\n]", phrase);
 
-    FILE *fptr = fopen("records.dat", "rb");
+    FILE *fptr = fopen("../data/message_storage.dat", "rb");
 
     if (fptr == NULL) {
         printf("Error opening files.\n");
-        return 1;
+        return;
     }
 
     Record rec;
@@ -52,35 +52,42 @@ int search_by_phrase() {
         if (strstr(rec.message, phrase)) {
             printf("ID: %d\n", rec.ID);
             printf("Title: %s\n", rec.title);
-            printf("Message: %s\n\n", rec.message);
+            printf("Message: %s\n", rec.message);
+            printf("Encrypted: %d\n\n", rec.is_encrypted);
+
+            ask_decrypt(rec);
+            found = 1;
         }
     }
-
+    if (!found){
+        printf("Message not Found");
+    }
+    
     fclose(fptr);
 
-    return 0;
+    return;
 }
 
-
 /*
-search_by_id
+search_by_id()
+-----------------------------------------
 Searches for a record by its unique ID.
-
 Prints the matching record.
 Returns 0 on success, 1 on error.
 */
-int search_by_id() {
 
+
+void search_by_id() {
     int id;
-
+    int found = 0;
     printf("Enter Message ID:");
     scanf("%d", &id);
 
-    FILE *fptr = fopen("records.dat", "rb");
+    FILE *fptr = fopen("../data/message_storage.dat", "rb");
 
     if (fptr == NULL) {
         printf("Error opening file.\n");
-        return 1;
+        return;
     }
 
     Record rec;
@@ -89,35 +96,39 @@ int search_by_id() {
         if (rec.ID == id) {
             printf("ID: %d\n", rec.ID);
             printf("Title: %s\n", rec.title);
-            printf("Message: %s\n\n", rec.message);
+            printf("Message: %s\n", rec.message);
+            printf("Encrypted: %d\n\n", rec.is_encrypted);
+
+            ask_decrypt(rec);
+            found = 1;
         }
+    }
+    if (!found){
+        printf("Message not Found");
     }
 
     fclose(fptr);
 
-    return 0;
+    return;
 }
 
-
 /*
-search_by_title
+search_by_title()
+-----------------------------------------
 Searches for a record by its title (case-sensitive).
-
 Prints the matching record.
 Returns 0 on success, 1 on error.
 */
-int search_by_title() {
-
+void search_by_title() {
+    int found = 0;
     char title[50];
-
     printf("Enter Message Title:");
     scanf(" %[^\n]", title);
 
-    FILE *fptr = fopen("records.dat", "rb");
-
+    FILE *fptr = fopen("../data/message_storage.dat", "rb");
     if (fptr == NULL) {
         printf("Error opening file.\n");
-        return 1;
+        return;
     }
 
     Record rec;
@@ -125,13 +136,41 @@ int search_by_title() {
     while (fread(&rec, sizeof(Record), 1, fptr)) {
         if (strcmp(rec.title, title) == 0) {
             printf("\nID: %d\n", rec.ID);
-            printf("Title: %s\n", rec.title);
-            printf("Message: %s\n\n", rec.message);
+            printf("Message: %s\n", rec.message);
+            printf("Encrypted: %d\n\n", rec.is_encrypted);
+
+            ask_decrypt(rec);
+            found = 1;
         }
     }
+    if (!found){
+        printf("Message not Found");
+    }
+    
 
     fclose(fptr);
 
-    return 0;
+    return;
+}
+
+//Asks user if they want to decrypt message then calls the decrypt function
+void ask_decrypt(Record rec) {
+    int dec;
+    char key[50];
+
+    printf("Do you want to decrypt message?\n1.Yes\n2.No\nEnter Choice: ");
+    scanf("%d", &dec);
+
+    if (dec == 1) {
+        if (rec.is_encrypted) {
+            printf("Enter Decryption Key: ");
+            scanf(" %[^\n]", key);
+            char *decrypted = decrypt(rec.message, key);
+            printf("Decrypted Message:\n%s\n", decrypted);
+            free(decrypted);
+        } else {
+            printf("This message is not encrypted.\n");
+        }
+    }
 }
 
