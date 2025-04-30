5e5d0e96dfa28b660eb45b76f8a6a50858d199e6
Marcus592-stack <kirandyal15@gmail.com>
2025-04-30 15:50:00 -0800

    Update censor.c

--- src/censor.c
+++ src/censor.c
@@ -6,118 +6,141 @@
 
 void end_of_menu();
 
-// Counts how many words are separated by commas
+
+/*
+count:
+Counts the number of comma-separated values in a message string.
+Used to determine how many words will be redacted.
+*/
 int count(char *message) {
-    int count = 1;  
+    int count = 1;
+
     while (*message) {
         if (*message == ',') {
             count++;
         }
         message++;
     }
+
     return count;
 }
 
-// Splits the message by commas
+
+/*
+list_maker:
+Takes a comma-separated string, trims whitespace from each token,
+and returns a dynamically allocated list of strings.
+
+Parameters:
+- message: input string (will be modified).
+- redacted_word_count: pointer to store the number of words parsed.
+
+Returns:
+- char**: an array of redacted words.
+*/
 char **list_maker(char *message, int *redacted_word_count) {
     int length = count(message);
     *redacted_word_count = length;
 
-    //creating list to hold censored words
-    char **new_list = malloc(length * sizeof(char*));
+    char **new_list = malloc(length * sizeof(char *));
     int index = 0;
-    //breaks the message up into tokens with "," as the separater
+
+    // Break the input into tokens separated by commas
     char *token = strtok(message, ",");
 
     while (token != NULL && index < length) {
-        // Trim leading/trailing whitespace
+        // Trim leading whitespace
         while (isspace(*token)) token++;
+
+        // Trim trailing whitespace
         char *end = token + strlen(token) - 1;
-        while (end > token && isspace(*end)) *end-- = '\0';
-        
-        //allocates memory for length of word + 1 for the EOL char
+        while (end > token && isspace(*end)) {
+            *end-- = '\0';
+        }
+
+        // Allocate memory and copy token
         new_list[index] = malloc(strlen(token) + 1);
-        //copies the word(token) into that memory allocated
         strcpy(new_list[index], token);
 
-        //increases the index
         index++;
-
-        //moves onto the next word
         token = strtok(NULL, ",");
     }
+
     return new_list;
 }
 
+
+/*
+redact:
+Replaces any word in the message that appears in the user-provided
+redacted list with asterisks (*).
+
+Parameters:
+- message: the original message to censor (modified in place).
+*/
 void redact(char *message) {
-    //declares array for input
-    char input[MAX_MESSAGE_SIZE]; 
-    printf("Enter comma separated list of words you want to censor:");
+    char input[MAX_MESSAGE_SIZE];
+
+    printf("Enter comma separated list of words you want to censor: ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
 
     int redacted_word_count;
     char **redacted = list_maker(input, &redacted_word_count);
+
     printf("Original: %s\n", message);
 
     char redacted_message[MAX_MESSAGE_SIZE];
     int i = 0;
     int j = 0;
-    //runs until it hits a EOL
+
     while (message[i]) {
-        //skips over spaces unless its the start of a word
         if (isalpha(message[i]) || message[i] == '-' || message[i] == '\'') {
-            //stores word
+            // Extract a word
             char word[100];
             int w = 0;
 
-            //while loop that loops until a space(gets a word)
             while (isalnum(message[i]) || message[i] == '-' || message[i] == '\'') {
                 word[w++] = message[i++];
             }
-            //adds end of sting char to word
+
             word[w] = '\0';
 
-            //redact flag to set if word should be redacted or not
+            // Determine if the word needs to be redacted
             int redact_flag = 0;
 
-            //checks if word is in redacted word
             for (int k = 0; k < redacted_word_count; k++) {
                 if (strcasecmp(word, redacted[k]) == 0) {
-                    //if word in redacted word list redact flag is set to 1
                     redact_flag = 1;
                     break;
                 }
             }
 
-            //loops through word
+            // Append either asterisks or the original word
             for (int m = 0; m < w; m++) {
-                //if redact flag is 1 it appends an esterisk
-                if(redact_flag){
+                if (redact_flag) {
                     redacted_message[j++] = '*';
-                }
-                //if not it appends the char
-                else{
+                } else {
                     redacted_message[j++] = word[m];
                 }
-                
             }
+
         } else {
             redacted_message[j++] = message[i++];
         }
     }
 
     redacted_message[j] = '\0';
-    //copies redacted message into message
+
     strcpy(message, redacted_message);
-    //prints the redacted code
     printf("Redacted: %s\n", message);
+
     end_of_menu();
 
-    //frees the pointers in redacted
+    // Free allocated memory
     for (int i = 0; i < redacted_word_count; i++) {
         free(redacted[i]);
     }
+
     free(redacted);
 }
-
