853d30a08c50ad4a30eaadc0617887892cf87b2f
RavindraPersaud <persaudr788@gmail.com>
2025-04-30 20:29:04 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,57 +13,71 @@ void modify();
 int load_messages();
 int store_message();
 
+/*Function: main
+----------------------------
+Launches the Notes Program and Display the main menu and get user's choice until they choose to exit
+Handle user's menu choice
+ Main menu options:
+0 – Store a new message (based on user selection)
+1 – View all stored messages (displays a preview of the messages)
+2 – Search for messages by title, ID, or content phrase
+3 – Modify an existing message (decrypts if necessary before editing)
+4 – Delete a message by ID or title
+5 – Exit 
+Each menu option calls a function defined in either the source(src) folders or th include folder. 
+Program exits on option 5.
 
+*/
 int main() {
     printf("WELCOME TO OUR NOTES PROGRAM!\nSee README.md for help.\n\n");
 
     int main_menu_choice;
 
     do {
-        // Display main menu and get user's choice
+        
         main_menu();
         main_menu_choice = user_choice();
         end_of_menu();
 
-        // Handle user's menu choice
+       
         switch (main_menu_choice) {
 
             case 0:
-                // Store a new note/message
+                
                 printf("Storing a new note/message.\n\n");
                 store_message();
                 end_of_menu();
                 break;
 
             case 1:
-                // View all stored messages
+               
                 printf("Viewing all notes/messages\n\n");
                 load_messages();
                 end_of_menu();
                 break;
 
             case 2: {
-                // Show search menu and perform searches
+                
                 int show_search_menu = 1;
                 int search_type;
-                //shows search menu
+                
                 while (show_search_menu == 1) {
                     search_menu();
                     search_type = user_choice();
                     end_of_menu();
-                    //searching by title
+                    
                     if (search_type == 1) {
                         printf("Searching by Title.\n");
                         search_by_title();
                         end_of_menu();
                     } 
-                    //searching by id
+                    
                     else if (search_type == 2) {
                         printf("Searching by ID.\n");
                         search_by_id();
                         end_of_menu();
                     } 
-                    //searching by phrase
+                    
                     else if (search_type == 3) {
                         printf("Search by phrase\n");
                         search_by_phrase();
@@ -84,26 +98,26 @@ int main() {
             }
 
             case 3:
-                // Modify a previously stored message
+                
                 printf("Modifying a previously stored note.\n");
                 modify();
                 end_of_menu();
                 break;
 
             case 4:
-                // Delete a message
+                
                 delete_menu();
                 end_of_menu();
                 break;
 
             case 5:
-                // Exit the program
+                
                 printf("Exiting program...\n");
                 end_of_menu();
                 return 0;
 
             default:
-                // Invalid menu input
+                
                 printf("Invalid input! Enter any number from 0 to 5.\n");
                 end_of_menu();
         }
